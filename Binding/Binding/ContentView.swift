97cae0fa1cc8b5df47b1c8f90913fa4d982b309a
menezesjovin <menezesjovin@gmail.com>
2020-04-28 17:25:30 +0530

    initial commit

---
+++ Binding/Binding/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Binding
+//
+//  Created by Jovin Menezes on 21/04/20.
+//  Copyright © 2020 Jovin Menezes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   @State var name : String = ""
+    var body: some View {
+    Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
